af63a0e37953596e21a02d6e89d2815f924d98e4
rugk <rugk+git@posteo.de>
2020-11-23 13:19:22 +0100

    Move warning to seperate message at the bottom

--- src/options/modules/CustomOptionTriggers.js
+++ src/options/modules/CustomOptionTriggers.js
@@ -5,17 +5,18 @@
  */
 
 import * as AutomaticSettings from "/common/modules/AutomaticSettings/AutomaticSettings.js";
-import * as CommonMessages from "/common/modules/MessageHandler/CommonMessages.js";
+import * as CustomMessages from "/common/modules/MessageHandler/CustomMessages.js";
 import { COMMUNICATION_MESSAGE_TYPE } from "/common/modules/data/BrowserCommunicationTypes.js";
 
 const TAB_FILTER_URLS = ["http://*/*", "https://*/*"];
+const MESSAGE_POSSIBLY_BROKEN_SETTING_STYLE = "optionWarningRareStyleSetting";
 
 /**
- * Send message to other parts of add-on to apply new setting.
+ * Applies the dark mode setting.
  *
  * @function
  * @private
- * @param  {boolean} optionValue
+ * @param  {string} optionValue
  * @param  {string} [option]
  * @returns {void}
  */
@@ -39,10 +40,53 @@ function applyFakedColorStatus(optionValue) {
 
     // show warning for rarely used values
     if (optionValue === "light" || optionValue === "no_preference") {
-        CommonMessages.showInfo("optionWarningRareStyleSetting");
+        CustomMessages.showMessage(MESSAGE_POSSIBLY_BROKEN_SETTING_STYLE, "optionWarningRareStyleSetting", false);
     } else {
-        CommonMessages.hideInfo();
+        CustomMessages.hideMessage(MESSAGE_POSSIBLY_BROKEN_SETTING_STYLE, {animate: true});
     }
+
+    broadcastNewSettings();
+}
+
+/**
+ * Applies the functional mode setting.
+ *
+ * @function
+ * @private
+ * @param  {boolean} optionValue
+ * @param  {string} [option]
+ * @returns {void}
+ */
+function applyFunctionalMode() {
+    broadcastNewSettings();
+}
+
+/**
+ * Send message to other parts of add-on to apply new setting.
+ *
+ * @function
+ * @private
+ * @param  {boolean} fakedColorStatus
+ * @param  {string} [option]
+ * @returns {void}
+ */
+function broadcastNewSettings(fakedColorStatus) {
+    const newSettingsMessage = {
+        type: COMMUNICATION_MESSAGE_TYPE.NEW_SETTING,
+        fakedColorStatus: fakedColorStatus
+    };
+
+    // send to all tabs
+    browser.tabs.query({
+        url: TAB_FILTER_URLS
+    }).then((tabs) => {
+        return Promise.all(tabs.map((tab) => {
+            browser.tabs.sendMessage(tab.id, newSettingsMessage);
+        }));
+    });
+
+    // send to own background script
+    browser.runtime.sendMessage(newSettingsMessage);
 }
 
 /**
@@ -55,7 +99,9 @@ function applyFakedColorStatus(optionValue) {
  */
 export function registerTrigger() {
     AutomaticSettings.Trigger.registerSave("fakedColorStatus", applyFakedColorStatus);
-    AutomaticSettings.Trigger.registerSave("functionalMode", applyFakedColorStatus);
+    AutomaticSettings.Trigger.registerSave("functionalMode", applyFunctionalMode);
+
+    CustomMessages.registerMessageType(MESSAGE_POSSIBLY_BROKEN_SETTING_STYLE, document.getElementById("messageWarningRareStyleSetting"));
 
     // handle loading of options correctly
     AutomaticSettings.Trigger.registerAfterLoad(AutomaticSettings.Trigger.RUN_ALL_SAVE_TRIGGER);
