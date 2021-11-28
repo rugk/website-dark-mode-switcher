ca2f2594a4eae90820ff4022d2b115586d6e2fdf
rugk <rugk+git@posteo.de>
2021-11-28 20:54:59 +0100

    Remove more dead code now

--- src/background/background.js
+++ src/background/background.js
@@ -1,8 +1,4 @@
-import "./modules/CssInjection.js";
-import * as CssAnalysis from "./modules/CssAnalysis.js";
 import * as ActionButton from "./modules/ActionButton.js";
 
 // init modules
-// CssInjection.init();
-CssAnalysis.init();
 ActionButton.init();
